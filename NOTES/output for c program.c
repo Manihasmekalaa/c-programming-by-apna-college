20d41d79996cd67b192a255d205c365bdc7a8c2d
Manihas <manihasmekala@gmail.com>
2025-12-31 23:15:48 +0530

    added some changes in how to run the code easily

---
+++ NOTES/output for c program.c
@@ -0,0 +1,25 @@
+// first open terminal 
+// check the file path 
+// if the file is not in the same folder 
+// go to file manager and right clcik and coply the path by opening the folder 
+// type in -----> cd "  paste the address here "
+// check if it shows the file now then
+// type gcc test.c -o test  then enter
+// type ./test 
+
+// you can the terminal now
+
+/*
+For running a C program in terminal:
+
+Without any hesitation 
+Download the exetension code runner 
+Then open the c file you want to run 
+Then press ctrl+alt+n 
+Then it will run the code and show the output in the terminal
+
+But you need to trun these setting
+1. go to settings
+2. search for code runner run in terminal
+3. then check the box
+*/
